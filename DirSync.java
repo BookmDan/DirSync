08c67d913f0eef79965ae3a74f72c446775ababf
BookmDan <danoh329@gmail.com>
2022-11-20 19:30:30 -0800

    first commit

--- DirSync.java
+++ DirSync.java
@@ -7,8 +7,54 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class DirSync {
+  private static String src;
+  private static String dest;
+
+  // default 
+  public void copyDir() {
+    copyDir(getSrc(), getDest(), true);
+  }
+
+  public void copyDir(String src, String dest, boolean overWrite) {
+    this.setSrc(src);
+    this.setDest(dest);
+
+    Directory
+
+  }
+
+  public static void main(String[] args) {
+
+
+    String[] paths;
+    try {
+      File file = new File("./temp1/tempFile.java");
+      // get all subdirectories
+      paths = file.list(); // list method returns array of strings of file directories
+      
+      if (!src.exists()) {
+        System.out.println("directory does not exist.");
+
+      }
+      else {
+
+      }
+      HashMap<String, String> subDir2 = new HashMap<String, String>();
+
+      for(String path : paths){
+        // store, copy src path files into dest path 
+       subDir2.put(dest, path);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+
+
   // parse files 
 
   // source 
@@ -17,4 +63,8 @@ public class DirSync {
 
 
   // call or return 
+
+  // copy files 
+  // copy dir
+
 }
---
+++ temp1/tempFile.java
@@ -0,0 +1,7 @@
+package DirSync.temp1;
+
+public class tempFile {
+  public static void main(String[] args) {
+    System.out.println("Hello");
+  }
+}
